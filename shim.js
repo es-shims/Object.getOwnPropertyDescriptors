e8efd6045c2027774b873ac7a28963b9433f2276
Jordan Harband <ljharb@gmail.com>
2016-07-06 00:31:33 -0700

    [Dev Deps] update `tape`, `jscs`, `nsp`, `eslint`, `@ljharb/eslint-config`, `semver`

--- shim.js
+++ shim.js
@@ -5,8 +5,10 @@ var define = require('define-properties');
 
 module.exports = function shimGetOwnPropertyDescriptors() {
 	var polyfill = getPolyfill();
-	define(Object, { getOwnPropertyDescriptors: polyfill }, {
-		getOwnPropertyDescriptors: function () { return Object.getOwnPropertyDescriptors !== polyfill; }
-	});
+	define(
+		Object,
+		{ getOwnPropertyDescriptors: polyfill },
+		{ getOwnPropertyDescriptors: function () { return Object.getOwnPropertyDescriptors !== polyfill; } }
+	);
 	return polyfill;
 };
